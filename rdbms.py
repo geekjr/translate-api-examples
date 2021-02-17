b3eab8f5ec545ed106fc2c97253bfadc89ec535a
ac ac <iamapythongeek@gmail.com>
2021-02-17 06:40:01 -0500

    Updated RDBMAS

---
+++ rdbms.py
@@ -0,0 +1,9 @@
+# In this example, we will use MySQL. Function is nearly identical for PostGRES.
+
+from translateAPI import translateAPI
+
+
+# This function(and the PostGRES one) return queries that have translated values. You can use these queries to insert
+# into another database.
+translated = translateAPI.mysql("URL_TO_SERVER", "USERNAME", "PASSWORD", "TABLE-NAME", "DATABASE NAME", "YOUR-API-KEY")
+print(translated)
